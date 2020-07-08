0a7a0cb930a036dc77059b01273f61f7a419b39b
Millan Garcia <millangg17@gmail.com>
2020-07-08 11:39:14 +0200

    task manager uncompleted

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/index', to: 'tasks#index'
-  get '/read', to: 'tasks#read'
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # post '/index', to: 'tasks#index'
 end
